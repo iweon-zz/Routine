81d22b6084a636e9675da9e7a850153d331b6981
iweon_zz <wldnjsdl0707@naver.com>
2025-11-18 16:15:04 +0900

    Initial Commit

---
+++ Routine/RoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineApp.swift
+//  Routine
+//
+//  Created by 김지원 on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct RoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RoutineTests/RoutineTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineTests.swift
+//  RoutineTests
+//
+//  Created by 김지원 on 11/18/25.
+//
+
+import Testing
+@testable import Routine
+
+struct RoutineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
